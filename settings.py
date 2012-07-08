b3ed6b56c44c0296340a66bcbc69423e53df7619
76house <martin.marek@carabana.cz>
2012-07-08 20:31:59 +0200

    settings.py warnings removed

--- settings.py
+++ settings.py
@@ -40,7 +40,7 @@ else:
 
     DATABASES = {
         'default': {
-            'ENGINE': 'sqlite3',             # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME':  os.path.join(os.path.dirname(__file__), 'db.sqlite'), # Or path to database file if using sqlite3.
             'USER': '',                      # Not used with sqlite3.
             'PASSWORD': '',                  # Not used with sqlite3.
