274625739e7a1d803838469529b9340c61726000
76house <martin.marek@carabana.cz>
2012-07-09 22:07:28 +0200

    robots.txt

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
 
 admin.autodiscover()
 
@@ -7,6 +8,9 @@ urlpatterns = patterns('',
 
     # administration
     (r'^admin/', include(admin.site.urls)),
+
+    # allow robots
+    (r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow:", mimetype="text/plain")),
 
     # carabana blog
     (r'^', include('blog.urls')),
